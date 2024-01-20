b62e04d3f372740c74fd7df9e67aeb5cd9e8ff05
Omar Tarek <omartarek611@outlook.com>
2024-01-20 18:15:52 +0200

    Add Card component to UserItem.jsx

--- src/user/components/UserItem.jsx
+++ src/user/components/UserItem.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./UserItem.css";
 import Avatar from "../../shared/components/UIElements/Avatar";
 import { Link } from "react-router-dom";
+import Card from "../../shared/components/UIElements/Card";
 
 const UserItem = (props) => {
 	return (
 		<li className="user-item">
-			<div className="user-item__content">
+			<Card className="user-item__content">
 				<Link to={`/${props.id}/places`}>
 					<div className="user-item__image">
 						<Avatar image={props.image} alt={props.name} />
@@ -18,7 +19,7 @@ const UserItem = (props) => {
 						</h3>
 					</div>
 				</Link>
-			</div>
+			</Card>
 		</li>
 	);
 };
