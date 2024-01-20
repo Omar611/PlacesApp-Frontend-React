d802a3f00c24fd05ad8e97b99c7f3070d299710f
Omar Tarek <omartarek611@outlook.com>
2024-01-20 23:26:00 +0200

    Refactor navigation component event handlers

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -9,18 +9,18 @@ import "./MainNavigation.css";
 
 const MainNavigation = () => {
 	const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-	const openDrawer = () => setDrawerIsOpen(true);
-	const closeDrawer = () => setDrawerIsOpen(false);
+	const openDrawerHandler = () => setDrawerIsOpen(true);
+	const closeDrawerHandler = () => setDrawerIsOpen(false);
 	return (
 		<React.Fragment>
-			{drawerIsOpen ? <Backdrop onClick={closeDrawer} /> : null}
-			<SideDrawer show={drawerIsOpen}>
+			{drawerIsOpen ? <Backdrop onClick={closeDrawerHandler} /> : null}
+			<SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
 				<nav className="main-navigation__drawer-nav">
 					<NavLinks />
 				</nav>
 			</SideDrawer>
 			<MainHeader>
-				<button className="main-navigation__menu-btn" onClick={openDrawer}>
+				<button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
 					<span></span>
 					<span></span>
 					<span></span>
