0644f224408eaba7d912d4287bf136c0a3c7b67e
Omar Tarek <omartarek611@outlook.com>
2024-01-26 23:03:30 +0200

    Change google maps to ol maps

--- src/shared/components/UIElements/Map.jsx
+++ src/shared/components/UIElements/Map.jsx
@@ -1,22 +1,47 @@
 import React, { useRef, useEffect } from "react";
+import MapOl from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import { fromLonLat } from "ol/proj";
+
+import "ol/ol.css";
+import "./Map.css";
 
 const Map = (props) => {
 	const mapRef = useRef();
+	const mapInstanceRef = useRef();
 
+	const { center, zoom } = props;
 	useEffect(() => {
-		const map = new window.google.maps.Map(mapRef.current, {
-			center: props.center,
-			zoom: props.zoom,
-		});
-
-		new window.google.maps.Marker({ position: props.center, map: map });
-	}, [props.center, props.zoom]);
+		if (!mapInstanceRef.current) {
+			mapInstanceRef.current = new MapOl({
+				target: mapRef.current.id,
+				layers: [
+					new TileLayer({
+						source: new OSM(),
+					}),
+				],
+				view: new View({
+					center: fromLonLat([center.lng, center.lat]),
+					zoom: zoom,
+				}),
+			});
+		} else {
+			// Update the map center and zoom level
+			mapInstanceRef.current
+				.getView()
+				.setCenter(fromLonLat([center.lng, center.lat]));
+			mapInstanceRef.current.getView().setZoom(zoom);
+		}
+	}, [center, zoom]);
 
 	return (
 		<div
 			ref={mapRef}
 			className={`map ${props.className}`}
-			style={props.style}></div>
+			style={props.style}
+			id="map"></div>
 	);
 };
 
